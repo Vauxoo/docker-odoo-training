e237c91a3776473a604944810bb3621ae813c8e6
Moisés López <moylop260@vauxoo.com>
2017-07-09 18:30:01 -0500

    [ADD] install

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:16.04
+
+ADD install.sh /install.sh
+RUN /install.sh
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y python-pip libxml2-dev libxslt-dev libevent-dev \
+    libsasl2-dev libldap2-dev python-lxml libjpeg-dev libsasl2-dev \
+    libssl-dev python-dev
+apt-get install -y tmux vim wkhtmltopdf git curl wget tmux unzip postgresql
+apt-get install locales && locale-gen "en_US.UTF-8"
+
+pip install -U pip
+
+curl http://j.mp/spf13-vim3 -L -o - | sh
+
+(cd /usr/bin && wget -qO- -t 1 --timeout=240 https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz | tar -xJ --strip-components=2 wkhtmltox/bin/wkhtmltopdf)
+
+/etc/init.d/postgresql start
+useradd -d /home/odoo -m -s /bin/bash -p odoopwd odoo
+
+su - postgres -c "createuser -s odoo"
+
+su - odoo -c "git clone https://github.com/odoo/odoo.git"
+su - odoo -c "pip install -r ~/odoo/requirements.txt"
+
+su - postgres -c "psql -c \"UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1'\""
+su - postgres -c "psql -c \"DROP DATABASE template1;\""
+su - postgres -c "psql -c \"CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING='UNICODE' LC_COLLATE='en_US.UTF8' LC_CTYPE='en_US.UTF8';\""
+su - postgres -c "psql -c \"UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';\""
+su - postgres -c "psql -c \"UPDATE pg_database SET datallowconn = FALSE WHERE datname = 'template1';\""
