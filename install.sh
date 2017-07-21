27352d14cc0a6f34ede1d53d15d37f10e94b580a
Moisés López <moylop260@vauxoo.com>
2017-07-21 11:11:40 -0500

    [REF] install: Configure 4 spaces vim ide and add comments

--- install.sh
+++ install.sh
@@ -19,22 +19,33 @@ update-locale LANG=en_US.UTF-8
 echo "LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 PYTHONIOENCODING=UTF-8" | tee -a /etc/bash.bashrc
 source /etc/bash.bashrc
 
+# Install after configure locales to auto-create cluster with UTF-8
 apt-get install -y postgresql
 
 pip install -U pip
-
-curl http://j.mp/spf13-vim3 -L -o - | sh
-
-(cd /usr/bin && wget -qO- -t 1 --timeout=240 https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz | tar -xJ --strip-components=2 wkhtmltox/bin/wkhtmltopdf)
 
 /etc/init.d/postgresql start
 useradd -d /home/odoo -m -s /bin/bash -p odoopwd odoo
 
 su - postgres -c "createuser -s odoo"
 
+# Download odoo
 su - odoo -c "git clone https://github.com/odoo/odoo.git"
+# Install odoo dependencies
 pip install -r /home/odoo/odoo/requirements.txt
-
 apt-get install -y npm
 ln -s /usr/bin/nodejs /usr/bin/node
 npm install -g less
+(cd /usr/bin && wget -qO- -t 1 --timeout=240 https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz | tar -xJ --strip-components=2 wkhtmltox/bin/wkhtmltopdf)
+
+# configure vim IDE
+curl http://j.mp/spf13-vim3 -L -o - | sh
+su - odoo -c "touch /home/odoo/.vimrc"
+echo -e """filetype plugin indent on
+\" show existing tab with 4 spaces width
+set tabstop=4
+\" when indenting with '>', use 4 spaces width
+set shiftwidth=4
+\" On pressing tab, insert 4 spaces
+set expandtab
+""" > /home/odoo/.vimrc
